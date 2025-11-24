32576ec089d21e7ca7b92ef276d4565a019451fc
Kyle-gi <root@DESKTOP-RD8DJJF.localdomain>
2025-11-24 09:40:06 +0100

    changes

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -9,13 +9,7 @@ public class main {
         boolean salir = false;
 
         while (!salir) {
-            System.out.println("\n=== AGENDA ===");
-            System.out.println("1. Listar");
-            System.out.println("2. Buscar por día");
-            System.out.println("3. Buscar por nombre");
-            System.out.println("4. Añadir");
-            System.out.println("5. Eliminar");
-            System.out.println("6. Salir");
+
 
             String opcion = sc.nextLine();
 
